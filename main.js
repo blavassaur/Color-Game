0b4c33ba920d1a941a0eb8b771277af5cafb90ae
Brian <blavassaur@gmail.com>
2018-11-14 20:29:45 +0900

    added random color generation

--- main.js
+++ main.js
@@ -1,17 +1,63 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var title = document.querySelector("h1");
+var messageDisplay = document.querySelector("#message");
+
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
+    //add colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squres
+    squares[i].addEventListener("click", function () {
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor
+        //compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "MY MAN!";
+            winColors(clickedColor);
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "lol nope";
+
+        }
+    })
+}
+
+//change color of squares when correct
+function winColors(color) {
+    //change h1 color
+    title.style.backgroundColor = color;
+    //change square color
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+
+    return arr;
+}
+
+function randomColor() {
+    //red
+    var red = Math.floor(Math.random() * 256);
+    //green
+    var green = Math.floor(Math.random() * 256);
+    //blue
+    var blue = Math.floor(Math.random() * 256);
+
+    return "rgb("+ red +", "+ green +", " + blue + ")";
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
